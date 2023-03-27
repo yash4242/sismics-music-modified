65e6a726d9561456c799583e93d7b3c140a5f1d9
jatinAgarwala <jatin.a@research.iiit.ac.in>
2023-03-28 00:16:32 +0530

    Add feature to convert output string to json for Spotify search (commented)

--- music-web/src/main/java/com/sismics/music/rest/searchfeature/strategies/SpotifySearch.java
+++ music-web/src/main/java/com/sismics/music/rest/searchfeature/strategies/SpotifySearch.java
@@ -2,37 +2,14 @@ package com.sismics.music.rest.searchfeature.strategies;
 
 import com.sismics.music.rest.searchfeature.SearchStrategy;
 
-import com.sismics.music.core.dao.dbi.AlbumDao;
-import com.sismics.music.core.dao.dbi.ArtistDao;
-import com.sismics.music.core.dao.dbi.TrackDao;
-import com.sismics.music.core.dao.dbi.criteria.AlbumCriteria;
-import com.sismics.music.core.dao.dbi.criteria.ArtistCriteria;
-import com.sismics.music.core.dao.dbi.criteria.TrackCriteria;
-import com.sismics.music.core.dao.dbi.dto.AlbumDto;
-import com.sismics.music.core.dao.dbi.dto.ArtistDto;
-import com.sismics.music.core.dao.dbi.dto.TrackDto;
-import com.sismics.music.core.util.dbi.PaginatedList;
-import com.sismics.music.core.util.dbi.PaginatedLists;
-import com.sismics.music.rest.util.JsonUtil;
-import com.sismics.rest.exception.ForbiddenClientException;
-import com.sismics.rest.util.Validation;
-
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Response;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Base64;
-import java.util.List;
+
+// import org.json.JSONObject;
 
 public class SpotifySearch implements SearchStrategy {
     public JsonObject search(String trackName) {
@@ -103,6 +80,7 @@ public class SpotifySearch implements SearchStrategy {
             }
         }
 
+        // JSONObject jsonObject = new JSONObject(response.toString());
         return null;
     }
 }
